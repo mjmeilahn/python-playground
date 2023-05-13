d21b9deb980eaea4e5f026cbf886a2f67c7bc6e7
Matthew Meilahn <12436547+mjmeilahn@users.noreply.github.com>
2023-05-13 16:01:07 -0700

    Create 3-aws-lambda.py

---
+++ 3-aws-lambda.py
@@ -0,0 +1,10 @@
+"""
+TO ADD LAYER(S) IN LAMBDA:
+1. Create Simple Folder e.g. mkdir requests (from pip install requests)
+2. cd requests
+3. Install individual packages:
+   pip install requests -t python/lib/python3.10/site-packages
+4. Go up one folder i.e. Get out of the current folder
+5. Zip the folder e.g. zip -r custom-file-name.zip folder-name
+6. Create Layer; Upload zipped file; Include Layer in Function.
+"""

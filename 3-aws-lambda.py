a68dde88b9dcf68b01bc50856cf7e36cb339ab68
Matthew Meilahn <12436547+mjmeilahn@users.noreply.github.com>
2023-05-25 16:25:25 -0700

    Update 3-aws-lambda.py

--- 3-aws-lambda.py
+++ 3-aws-lambda.py
@@ -4,9 +4,8 @@ TO ADD LAYER(S) IN LAMBDA:
 2. cd requests
 3. Install individual packages:
    pip install requests -t python/lib/python3.10/site-packages
-4. Go into the /requests folder. The /python folder should be a child.
-5. Zip the folder e.g. zip -r python.zip python
-6. Create Layer; Upload zipped file; Set correct run-time and architecture.
-7. Include Layer in Function and set the correct version number.
-8. Any manual layer uploads beyond here require a manual version number adjustment in the function.
+4. Zip the python folder e.g. zip -r python.zip python
+5. Create Layer; Upload zipped file; Set correct run-time and architecture.
+6. Include Layer in Function and set the correct version number.
+7. Any manual layer uploads beyond here require a manual version number adjustment in the function.
 """
